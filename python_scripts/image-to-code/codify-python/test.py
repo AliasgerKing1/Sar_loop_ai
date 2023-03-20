26b3d2b721c72dc5f4adc4137b7e17c282401d4b
AliasgerKing1 <aliasgersub34.com>
2023-03-20 12:12:33 +0530

    update

--- python_scripts/image-to-code/codify-python/test.py
+++ python_scripts/image-to-code/codify-python/test.py
@@ -10,7 +10,7 @@ import math
 
 # Load the input image and convert it to grayscale
 # input_img = cv2.imread(input_path)
-input_img = cv2.imread("image.png")
+input_img = cv2.imread("div2.png")
 gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
 
 # Apply a threshold to the grayscale image to get a binary image
@@ -28,27 +28,11 @@ for contour in contours:
     if len(approx) == 4:
         x, y, w, h = cv2.boundingRect(contour)
         ct.append([x, y, w, h])
-    
+    elif len(approx) == 3:
+            x, y, w, h = cv2.boundingRect(contour)
+            ct.append([x, y, w, h])
+    print(len(approx))
 
 # Reverse the order of the rectangles and output as JSON-formatted string
 ct.pop(-1)
 ct.reverse()
-# print(json.dumps(ct))
-# path = os.path.abspath("code")
-div_temp = "<div style='height:#h#px;width:#w#px;background-color:rgb(85,25,#c#); position : fixed; left : #x#px; top : #y#px;'></div>"
-div_str = ""
-for i in ct:
-    div_str+=div_temp.replace("#h#", str(i[3])).replace("#w#", str(i[2])).replace("#x#", str(i[0])).replace("#y#", str(i[1])).replace("#c#", str(i[0]*10))
-with open("code/index.html", "w") as wFile :
-    wFile.write("""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body style="position : relative">
-    #div# 
-</body>
-</html>""".replace("#div#", div_str))
